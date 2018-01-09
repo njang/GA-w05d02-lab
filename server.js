841bce5e0e48ceb85fa31ba3d11897bc2e640954
njang <neo.w.jang@gmail.com>
2018-01-09 16:41:07 -0600

    Update server.js to use db and get all books

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 //  SETUP and CONFIGURATION
 /////////////////////////////
 
+var db = require('./models')
 
 //require express in our app
 var express = require('express'),
@@ -24,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var port = 3000;
 
 // models/index.js
-module.exports.Book = require("./book.js");
+module.exports.Book = require("./models/book.js");
 
 
 
@@ -32,32 +33,32 @@ module.exports.Book = require("./book.js");
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
+// var books = [
+//   {
+//     _id: 15,
+//     title: "The Four Hour Workweek",
+//     author: "Tim Ferriss",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
+//     release_date: "April 1, 2007"
+//   },
+//   {
+//     _id: 16,
+//     title: "Of Mice and Men",
+//     author: "John Steinbeck",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
+//     release_date: "Unknown 1937"
+//   },
+//   {
+//     _id: 17,
+//     title: "Romeo and Juliet",
+//     author: "William Shakespeare",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
+//     release_date: "Unknown 1597"
+//   }
+// ];
 
 
-var newBookUUID = 18;
+// var newBookUUID = 18;
 
 
 
@@ -80,8 +81,15 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  // console.log('books index');
+  // res.json(books);
+  db.Book.find(function(err, books){
+    if (err) {
+      console.log("index error: " + err);
+      res.sendStatus(500);
+    }
+    res.json(books);
+  });
 });
 
 // get one book
